5641c21834f02d76d3b467a3d88d09247f313bf9
Pandarock0 <151137645+Pandarock0@users.noreply.github.com>
2023-11-27 00:04:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from functions import *
 
-
-#Chemin accès pour les fonctions "president_name" et "copy"
+#Chemin accès 
 chemin_acces = 'speeches'
 extension = '.txt'
 cleaned_folder = 'cleaned'
@@ -12,9 +11,50 @@ list = president_name(chemin_acces, extension)
 print("List of the names of all the president presents in the folder speeches :", list)
 
 #Chemin accès pour la fonction "punctuation"
-chemin_accès = punctuation('cleaned')
-print(chemin_accès)
 
-#Occurence number
-chemin_acc = 'Dictionnary'
-occurence = occurence(chemin_acc)
+
+
+#calcul tf-idf
+lien = 'cleaned'
+Matrix(lien)
+punctuation(lien)
+
+#Question 1 et 2
+
+matrix = Matrix(lien)
+useless_words(matrix)
+strong_words(matrix)
+
+'''Question 3
+As we can see in the file "Most_Common_Chirac_Words.txt"
+His most common words are :
+de: 97
+la: 64
+l: 48
+et: 46
+les: 38
+le: 36
+à: 32
+d: 29
+une: 28
+des: 22
+'''
+lot_words_chirac()
+
+
+#print(least_important_words(Matrix))
+
+'''Question 4:
+As we can see in the file "Most_Mentions_of_Nation.txt" the president who said the word "nation" are :
+
+'''
+
+
+nation()
+
+#Question 5 
+
+climat()
+
+#Question 6 
+common_words_all_presidents()
