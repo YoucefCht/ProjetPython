9230b305958601ec7839fe45a80af0496ad57527
Pandarock0 <151137645+Pandarock0@users.noreply.github.com>
2023-11-27 00:03:44 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,38 +1,65 @@
+import math
 import os
 
 
-def president_name(directory,
-                   extension):  # fonctions pour sortir les documents d'un fichier et donner une liste des présidents présents
+def president_name(directory,extension):  
+# functions to output documents from a file and provide a list of presidents 
+  
     files_names = set()
-    for files in os.listdir(directory):  # lit les fichiers contenue dans la variable (directory)
-        if files.endswith(extension):  # seulement si le fichier est de type de la variable extension (.txt.py ...)
-            character_to_remove = ["Nomination_", ".txt", "1", "2"]  # enlève les caractères gênants
+    # set to store the names of the presidents
+  
+    for files in os.listdir(directory):  
+    # reads the files contained in the variable directory
+      
+        if files.endswith(extension):  
+        # only if file is from the variable extension (.txt.py ...)
+          
+            character_to_remove = ["Nomination_", ".txt", "1", "2"]  
+            # List of annoying features to remove
+          
             for value in character_to_remove:
                 files = files.replace(value, "")
+            # removes annoying characters
 
             files_names.add(files)
+            # add the name of the president to the set
+  
     return list(files_names)
+    # return the list of presidents
 
 
 def copy(directory, extension, cleaned_folder):
+# function that copies files to a folder taking three parameters :
+# directory (which contains the files to be copied), 
+# extension (which specifies the file type to be copied)
+# cleaned_folder (which is the destination folder where the files will be copied)
+  
     for files in os.listdir(directory):
         if files.endswith(extension):
             input_path = os.path.join(directory, files)
-            output_path = os.path.join(cleaned_folder, files.lower())  # replace 'filename' with 'files'
+            # 
+            output_path = os.path.join(cleaned_folder, files.lower())  
+            # replace 'filename' with 'files'
+          
             with open(input_path, "r") as f, open(output_path, "w") as cleaned:
                 contenue = f.read()
-                for value in contenue:
+                for value in contenue:                  
                     if 65 <= ord(value) <= 90:
                         value = chr(ord(value) + 32)
                         cleaned.write(value)
+          #For each letter in "contenue" transforms uppercase into lowercase     
                     else:
                         cleaned.write(value)
 
 
 def punctuation(chemin):
+  # fonction qui supprime tout les caractères spéciaux de 
+
     ponct = [',', '.', ';', ':', '!', '?', '(', ')', '[', ']', '{', '}', '<', '>', '/', '\\', '|', '@', '#', '$', '%',
              '^', '&', '*', '_', '+', '=', '`', '~', '"']
-    ponct2 = ["'", "-"]
+    # List of special characters to be deleted
+    ponct2 = ["'", "-", chr(10)]
+    # List of special characters to be replaced by a space
 
     for filename in os.listdir(chemin):
         if filename.endswith(".txt"):
@@ -43,9 +70,281 @@ def punctuation(chemin):
 
             for value in ponct:
                 content = content.replace(value, "")
+  # supprime les caractères spéciaux de la première list
 
             for value in ponct2:
                 content = content.replace(value, " ")
+# remplace les caractères spéciaux de la deuxième liste par des espaces
+            file_path = os.path.join(chemin, filename)
 
             with open(file_path, "w") as file:
                 file.write(content)
+
+
+
+
+def function_tf(lien):
+  # function that do the tf calculation
+  for filename in os.listdir(lien):
+      if filename.endswith(".txt"):
+          file_path = os.path.join(lien, filename)
+
+          with open(file_path, 'r') as f1, open('Dictionnary/Dico_TF_'+filename, 'w') as f2:
+              content = f1.read()
+
+              word_count = {}
+              words = content.split()  #split the contentt in a list of words
+
+              for word in words:
+                  if word not in word_count:
+                      word_count[word] = 1
+                  else:
+                      word_count[word] += 1
+
+              for word, count in word_count.items():
+                  f2.write(f"{word}: {count}\n")
+                #count the times a number apear
+
+def count(directory):
+  w_d_count = {}
+  for filename in os.listdir(directory):
+      if filename.endswith(".txt"):
+          file_path = os.path.join(directory, filename)
+
+          with open(file_path, 'r') as file:
+              content = file.read()
+              words = set(content.split()) 
+
+              for word in words:
+                  if word not in w_d_count:
+                      w_d_count[word] = 1
+                  else:
+                      w_d_count[word] += 1
+  return w_d_count
+  #count the times a number apear
+
+
+
+def function_idf(directory, output_file='idf_scores.txt'):
+  # function that do the calcullation of idf
+    total_documents = len(os.listdir(directory))
+    w_d_count = {}
+
+   
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):
+            file_path = os.path.join(directory, filename)
+
+            with open(file_path, 'r') as file:
+                content = file.read()
+                words = set(content.split())
+                for word in words:
+                    w_d_count[word] = w_d_count.get(word, 0) + 1
+
+    # Calcul des scores IDF pour chaque mot
+    idf_scores = {}
+    for word, count in w_d_count.items():
+        idf_scores[word] = math.log10(total_documents / count) if count != 0 else 0
+
+    # Écriture du dictionnaire dans un fichier texte
+    with open(output_file, 'w') as file:
+        for word, score in idf_scores.items():
+            file.write(f"{word}: {score}\n")
+
+    return idf_scores
+
+
+
+
+
+def Matrix(directory, output_file='tf_idf.txt'):
+  total_documents = len(os.listdir(directory))
+  IDF = function_idf(directory)
+  tf_idf_matrix = {}
+  #récolte de tout les mots
+  all_words = set()
+  words_in_files = {file: set() for file in os.listdir(directory)}
+  
+  for file in os.listdir(directory):
+      with open(os.path.join(directory, file), 'r', encoding='utf-8') as file_content:
+          words = set(file_content.read().split())
+          all_words.update(words)
+          words_in_files[file] = words
+# annonce de la matrice
+  for word in all_words:
+      tf_idf_matrix[word] = [0] * total_documents
+  for file_idx, file in enumerate(os.listdir(directory)):
+      words_in_file = words_in_files[file]
+      for word in words_in_file:
+          tfidf = float(IDF.get(word, 0))
+          tf_idf_matrix[word][file_idx] = tfidf
+  # matrice tf-idf dans un txt
+  with open(output_file, 'w') as file:
+      for word, tfidf_scores in tf_idf_matrix.items():
+          scores_str = ' '.join(map(str, tfidf_scores))
+          if any(tfidf_scores):
+              file.write(f"{word}: {scores_str}\n")
+  
+  return tf_idf_matrix
+#retour de la matric tf-idf
+def useless_words(tf_idf_matrix, output_file='useless.txt'):
+  useless_words = []
+
+  for word, tf_idf_scores in tf_idf_matrix.items():
+      total_score = sum(tf_idf_scores)
+      if total_score == 0:
+          useless_words.append(word)
+
+  with open(output_file, 'w') as file:
+      for word in useless_words:
+          file.write(f"{word}\n")
+
+def strong_words(tf_idf_matrix, output_file='strong word.txt'):
+  strong_words = {}
+
+  for word, tf_idf_scores in tf_idf_matrix.items():
+      max_score = max(tf_idf_scores)
+      if max_score > 0:
+          strong_words[word] = max_score
+
+  sorted_words = sorted(strong_words.items(), key=lambda x: x[1], reverse=True)
+
+  with open(output_file, 'w') as file:
+      for word, score in sorted_words:
+          file.write(f"{word}: {score}\n")
+
+
+def lot_words_chirac():
+  chirac_files = ["Dictionnary/Dico_TF_nomination_chirac1.txt", "Dictionnary/Dico_TF_nomination_chirac2.txt"]
+  chirac_words = {}
+  
+  # Fusionne les deux fichiers pour obtenir un seul dictionnaire avec la somme des fréquences des mots
+  for file_path in chirac_files:
+      with open(file_path, 'r') as file:
+          for line in file:
+              word, count = line.split(": ")
+              chirac_words[word] = chirac_words.get(word, 0) + int(count)
+  
+  # Trie les mots par fréquence décroissante
+  sorted_words = sorted(chirac_words.items(), key=lambda x: x[1], reverse=True)
+  
+  # Sélectionne les 10 mots les plus fréquents
+  top_words = sorted_words[:10]
+  
+  # Écrit les mots les plus fréquents dans un nouveau fichier
+  with open("Most_Common_Chirac_Words.txt", 'w') as output_file:
+      for word, count in top_words:
+          output_file.write(f"{word}: {count}\n")
+  
+  return top_words
+
+def nation():
+  presidents = {
+      "Chirac": ["Dictionnary/Dico_TF_nomination_chirac1.txt", "Dictionnary/Dico_TF_nomination_chirac2.txt"],
+      "Giscard d'Estaing": ["Dictionnary/Dico_TF_nomination_giscard destaing.txt"],
+      "Hollande": ["Dictionnary/Dico_TF_nomination_hollande.txt"],
+      "Macron": ["Dictionnary/Dico_TF_nomination_macron.txt"],
+      "Mitterrand": ["Dictionnary/Dico_TF_nomination_mitterrand1.txt", "Dictionnary/Dico_TF_nomination_mitterrand2.txt"],
+      "Sarkozy": ["Dictionnary/Dico_TF_nomination_sarkozy.txt"]
+  }
+
+  word_count = {}
+  for president, files in presidents.items():
+      word_count[president] = 0
+      for file_path in files:
+          with open(file_path, 'r') as file:
+              for line in file:
+                  word, count = line.split(": ")
+                  if word.lower() == 'nation':
+                      word_count[president] += int(count)
+
+  # Tri des présidents par nombre de mentions décroissant
+  sorted_presidents = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
+
+  # Écriture des résultats dans un fichier
+  with open('Most_Mentions_of_Nation.txt', 'w') as output_file:
+      output_file.write("Président(s) et mentions du mot 'Nation':\n")
+      for president, mentions in sorted_presidents:
+          output_file.write(f"{president}: {mentions} mentions\n")
+
+def climat():
+  presidents = {
+      "Chirac": ["Dictionnary/Dico_TF_nomination_chirac1.txt", "Dictionnary/Dico_TF_nomination_chirac2.txt"],
+      "Giscard d'Estaing": ["Dictionnary/Dico_TF_nomination_giscard destaing.txt"],
+      "Hollande": ["Dictionnary/Dico_TF_nomination_hollande.txt"],
+      "Macron": ["Dictionnary/Dico_TF_nomination_macron.txt"],
+      "Mitterrand": ["Dictionnary/Dico_TF_nomination_mitterrand1.txt", "Dictionnary/Dico_TF_nomination_mitterrand2.txt"],
+      "Sarkozy": ["Dictionnary/Dico_TF_nomination_sarkozy.txt"]
+  }
+
+  first_mention = {}
+  for president, files in presidents.items():
+      first_mention[president] = float('inf')
+      for file_path in files:
+          with open(file_path, 'r') as file:
+              for line in file:
+                  word, _ = line.split(": ")
+                  if word.lower() in ['climat', 'écologie']:
+                      current_mention = files.index(file_path) + 1
+                      if current_mention < first_mention[president]:
+                          first_mention[president] = current_mention
+
+  # Recherche du président qui a mentionné ces mots en premier
+  first_president_mention = min(first_mention, key=first_mention.get)
+  
+
+  with open('First_Mention_Climate_Ecology.txt', 'w') as output_file:
+      output_file.write(f"Le premier président à mentionner 'climat' ou 'écologie' est {first_president_mention}.\n")
+
+
+
+def common_words_all_presidents():
+  presidents = {
+      "Chirac": ["Dictionnary/Dico_TF_nomination_chirac1.txt", "Dictionnary/Dico_TF_nomination_chirac2.txt"],
+      "Giscard d'Estaing": ["Dictionnary/Dico_TF_nomination_giscard destaing.txt"],
+      "Hollande": ["Dictionnary/Dico_TF_nomination_hollande.txt"],
+      "Macron": ["Dictionnary/Dico_TF_nomination_macron.txt"],
+      "Mitterrand": ["Dictionnary/Dico_TF_nomination_mitterrand1.txt", "Dictionnary/Dico_TF_nomination_mitterrand2.txt"],
+      "Sarkozy": ["Dictionnary/Dico_TF_nomination_sarkozy.txt"]
+  }
+
+  # Stockage des mots de chaque président
+  words_per_president = {president: set() for president in presidents}
+
+  # Lecture des dictionnaires pour chaque président
+  for president, files in presidents.items():
+      for file_path in files:
+          with open(file_path, 'r') as file:
+              for line in file:
+                  word, _ = line.split(": ")
+                  words_per_president[president].add(word)
+
+  # Trouver l'intersection des mots entre tous les présidents
+  common_words = set.intersection(*words_per_president.values())
+
+  # Écriture des mots communs dans un fichier
+  with open('Common_Words_All_Presidents.txt', 'w') as output_file:
+      output_file.write("Mots communs à tous les présidents :\n")
+      for word in common_words:
+          output_file.write(f"{word}\n")
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
